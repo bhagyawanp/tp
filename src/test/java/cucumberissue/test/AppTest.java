7cd74e07746b06107de21911f619bd0720d3dd2f
technogeekslucky <technogeekslucky@gmail.com>
2022-07-13 10:16:38 +0530

    test

---
+++ src/test/java/cucumberissue/test/AppTest.java
@@ -0,0 +1,20 @@
+package cucumberissue.test;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
